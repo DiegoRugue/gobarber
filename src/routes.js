7f1a50ce3c12c4b5faaab8e5408fa654a88539a5
Diego Rugue <diego.rugue>
2019-11-04 14:14:36 -0300

    upload file

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import configMulter from './config/multer';
 import actionFilter from './middleweres/actionfilter';
 import auth from './middleweres/auth';
 
@@ -6,10 +8,14 @@ import UserController from './core/user/controller';
 import SessionController from './core/session/controller';
 
 const routes = new Router();
+const upload = multer(configMulter);
 
 // Routes without authentication
 routes.post('/users', actionFilter(UserController.store));
 routes.post('/session', actionFilter(SessionController.store));
+routes.post('/file', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 routes.use(auth);
 
